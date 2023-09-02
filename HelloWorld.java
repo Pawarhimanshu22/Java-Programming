9fb1777d50bbf95e76b8d94631ac9aae0624f170
Pawarhimanshu22 <himanshupawarofficial2203@gmail.com>
2023-09-02 15:50:39 +0530

    HelloWorld.java modified added m2()

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,5 +4,13 @@ Class HelloWorld
  {
         system.out.println("Hello World");
         system.out.println("Hello Himanshu");
+        ///code1
+  }
+ }
+public static void main(String[] args)
+ {
+        system.out.println("Hello World");
+        system.out.println("Hello Himanshu");
+        ///code2
   }
  }
